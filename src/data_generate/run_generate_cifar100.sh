2aadc57f6aa6327ae3b7e4010ab7c5aa30648338
ds-ai96 <jener05458@gmail.com>
2024-05-27 15:51:57 +0900

    240527 commit (2)

--- src/data_generate/run_generate_cifar100.sh
+++ src/data_generate/run_generate_cifar100.sh
@@ -1,9 +1,12 @@
-python generate_data.py \
-        --model=resnet34_cifar100 \
-        --batch_size=256 \
-        --test_batch_size=512 \
-        --group=1 \
-        --beta=0.1 \
-        --gamma=0.5 \
-        --save_path_head=../data/cifar100 \
-        --lbns False
+for g in 1 2 3 4
+do
+python generate_data.py 		\
+		--model=resnet18 	 \
+		--batch_size=256 		\
+		--test_batch_size=512 \
+		--group=$g \
+		--beta=0.1 \
+		--gamma=0.5 \
+		--save_path_head=../data/cifar \
+		--lbns True 
+done
