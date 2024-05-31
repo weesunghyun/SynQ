f0dad9d4c54dc3689f01848d5cabc54e72f1c026
ds-ai96 <jener05458@gmail.com>
2024-05-31 15:29:59 +0900

    cleaning codes for pylint

--- src/utils/log_print.py
+++ src/utils/log_print.py
@@ -1,82 +1,140 @@
-from termcolor import colored
-import numpy as np
 import datetime
 
+import numpy as np
 
-__all__ = ["compute_remain_time", "print_result", "print_weight", "print_grad"]
+
+# __all__ = ["compute_remain_time", "print_result", "print_weight", "print_grad"]
+__all__ = ["compute_remain_time", "print_result"]
 
 
-single_train_time = 0
-single_test_time = 0
-single_train_iters = 0
-single_test_iters = 0
+SINGLE_TRAIN_TIME = 0
+SINGLE_TEST_TIME = 0
+SINGLE_TRAIN_ITERS = 0
+SINGLE_TEST_ITERS = 0
 
 
 def compute_remain_time(epoch, nEpochs, count, iters, data_time, iter_time, mode="Train"):
-    global single_train_time, single_test_time
-    global single_train_iters, single_test_iters
+    """
+        Compute the remain time for training or testing
+        Args:
+            epoch: current epoch
+            nEpochs: total epochs
+            count: current iteration
+            iters: total iterations
+            data_time: data loading time
+            iter_time: iteration time
+            mode: train or test
+        Returns:
+            time_str: time string
+            total_time: total time
+            left_time: left time
+    """
+    global SINGLE_TRAIN_TIME, SINGLE_TEST_TIME
+    global SINGLE_TRAIN_ITERS, SINGLE_TEST_ITERS
 
     # compute cost time
     if mode == "Train":
-        single_train_time = single_train_time * \
+        SINGLE_TRAIN_TIME = SINGLE_TRAIN_TIME * \
                             0.95 + 0.05 * (data_time + iter_time)
-        # single_train_time = data_time + iter_time
-        single_train_iters = iters
-        train_left_iter = single_train_iters - count + \
-                          (nEpochs - epoch - 1) * single_train_iters
-        # print "train_left_iters", train_left_iter
-        test_left_iter = (nEpochs - epoch) * single_test_iters
-    else:
-        single_test_time = single_test_time * \
-                           0.95 + 0.05 * (data_time + iter_time)
-        # single_test_time = data_time+iter_time
-        single_test_iters = iters
-        train_left_iter = (nEpochs - epoch - 1) * single_train_iters
-        test_left_iter = single_test_iters - count + \
-                         (nEpochs - epoch - 1) * single_test_iters
 
-    left_time = single_train_time * train_left_iter + \
-                single_test_time * test_left_iter
-    total_time = (single_train_time * single_train_iters +
-                  single_test_time * single_test_iters) * nEpochs
-    time_str = "TTime: {}, RTime: {}".format(datetime.timedelta(seconds=total_time),
-                                                           datetime.timedelta(seconds=left_time))
+        SINGLE_TRAIN_ITERS = iters
+        train_left_iter = SINGLE_TRAIN_ITERS - count + \
+                          (nEpochs - epoch - 1) * SINGLE_TRAIN_ITERS
+
+        test_left_iter = (nEpochs - epoch) * SINGLE_TEST_ITERS
+    else:
+        SINGLE_TEST_TIME = SINGLE_TEST_TIME * \
+                           0.95 + 0.05 * (data_time + iter_time)
+
+        SINGLE_TEST_ITERS = iters
+        train_left_iter = (nEpochs - epoch - 1) * SINGLE_TRAIN_ITERS
+        test_left_iter = SINGLE_TEST_ITERS - count + \
+                         (nEpochs - epoch - 1) * SINGLE_TEST_ITERS
+
+    left_time = SINGLE_TRAIN_TIME * train_left_iter + \
+                SINGLE_TEST_TIME * test_left_iter
+    total_time = (SINGLE_TRAIN_TIME * SINGLE_TRAIN_ITERS +
+                  SINGLE_TEST_TIME * SINGLE_TEST_ITERS) * nEpochs
+    time_str = (
+        f"TTime: {datetime.timedelta(seconds=total_time)}, "
+        f"RTime: {datetime.timedelta(seconds=left_time)}"
+    )
+
     return time_str, total_time, left_time
 
 
-def print_result(epoch, nEpochs, count, iters, lr, data_time, iter_time, error, loss, top5error=None,
+def print_result(epoch, nEpochs, count, iters, lr, data_time,
+                 iter_time, error, loss, top5error=None,
                  mode="Train", logger=None):
-    log_str = ">>> {}: [{:0>3d}|{:0>3d}], Iter: [{:0>3d}|{:0>3d}], LR: {:.6f}, DataTime: {:.4f}, IterTime: {:.4f}, ".format(
-        mode, epoch + 1, nEpochs, count, iters, lr, data_time, iter_time)
+    """
+        Print the training or testing result
+        Args:
+            epoch: current epoch
+            nEpochs: total epochs
+            count: current iteration
+            iters: total iterations
+            lr: learning rate
+            data_time: data loading time
+            iter_time: iteration time
+            error: error rate
+            loss: loss
+            top5error: top5 error rate
+            mode: train or test
+            logger: logger
+        Returns:
+            total_time: total time
+            left_time: left time
+        """
+
+    log_str = (
+        f">>> {mode}: [{epoch + 1:0>3d}|{nEpochs:0>3d}], Iter: [{count:0>3d}|{iters:0>3d}], "
+        f"LR: {lr:.6f}, DataTime: {data_time:.4f}, IterTime: {iter_time:.4f},"
+    )
+
     if isinstance(error, list) or isinstance(error, np.ndarray):
-        for i in range(len(error)):
-            log_str += "Error_{:d}: {:.4f}, Loss_{:d}: {:.4f}, ".format(i, error[i], i, loss[i])
+        for i, error_i in enumerate(error):
+            log_str += f"Error_{i}: {error_i:.4f}, Loss_{i}: {loss[i]:.4f}, "
+
     else:
-        log_str += "Error: {:.4f}, Loss: {:.4f}, ".format(error, loss)
+        log_str += f"Error: {error:.4f}, Loss: {loss:.4f}, "
 
     if top5error is not None:
         if isinstance(top5error, list) or isinstance(top5error, np.ndarray):
-            for i in range(len(top5error)):
-                log_str += " Top5_Error_{:d}: {:.4f}, ".format(i, top5error[i])
-        else:
-            log_str += " Top5_Error: {:.4f}, ".format(top5error)
+            for i, top5error_i in enumerate(top5error):
+                log_str += f"Top5_Error_{i}: {top5error_i:.4f}, "
 
-    time_str, total_time, left_time = compute_remain_time(epoch, nEpochs, count, iters, data_time, iter_time, mode)
+        else:
+            log_str += f" Top5_Error: {top5error:.4f}, "
+
+    time_str, total_time, left_time = compute_remain_time(epoch, nEpochs, count, iters,
+                                                          data_time, iter_time, mode)
 
     logger.info(log_str + time_str)
 
     return total_time, left_time
 
 
-def print_weight(layers, logger):
-    if isinstance(layers, MD.qConv2d):
-        logger.info(layers.weight)
-    elif isinstance(layers, MD.qLinear):
-        logger.info(layers.weight)
-        logger.info(layers.weight_mask)
-    logger.info("------------------------------------")
+# def print_weight(layers, logger):
+#     """
+#         Print the weight of the layers
+#         Args:
+#             layers: target layers
+#             logger: logger
+#     """
+#     if isinstance(layers, MD.qConv2d):
+#         logger.info(layers.weight)
+#     elif isinstance(layers, MD.qLinear):
+#         logger.info(layers.weight)
+#         logger.info(layers.weight_mask)
+#     logger.info("------------------------------------")
 
 
-def print_grad(m, logger):
-    if isinstance(m, MD.qLinear):
-        logger.info(m.weight.data)
+# def print_grad(m, logger):
+#     """
+#         Print the grad of the module
+#         Args:
+#             m: target module
+#             logger: logger
+#     """
+#     if isinstance(m, MD.qLinear):
+#         logger.info(m.weight.data)
