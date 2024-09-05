941b39850fd409973d074cd6cadb5fedb77fd51f
ds-ai96 <jener05458@gmail.com>
2024-09-05 17:05:28 +0900

    first build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,10 @@ author = 'Minjun Kim'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',  # 코드에서 자동으로 문서를 생성
+    'sphinx.ext.napoleon'  # Google style docstring 및 Numpy style docstring을 지원
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
