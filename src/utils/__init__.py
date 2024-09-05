304653dbf7ce54122bfb2dd31a794641c9fc42a6
ds-ai96 <jener05458@gmail.com>
2024-09-05 22:08:32 +0900

    Commit: minor update

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,24 @@
+"""
+[SW Starlab]
+Zero-shot Quantization with SynQ (Synthesis-aware Fine-tuning for Zero-shot Quantization)
+
+Author: Minjun Kim (minjun.kim@snu.ac.kr), Seoul National University
+        Jongjin Kim (j2kim99@snu.ac.kr), Seoul National University
+        U Kang (ukang@snu.ac.kr), Seoul National University
+
+Version : 1.0
+Date : Sep 6th, 2023
+Main Contact: Minjun Kim
+This software is free of charge under research purposes.
+For commercial purposes, please contact the authors.
+
+__init__.py
+    - codes for importing all utility functions
+
+This code is mainly based on
+    - ZeroQ: https://github.com/amirgholami/ZeroQ
+    - HAST: https://github.com/lihuantong/HAST
+"""
 from utils.lr_policy import *
 from utils.compute import *
 from utils.log_print import *
