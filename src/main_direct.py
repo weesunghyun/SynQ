44f581798c389767030870b719a19acd7319e287
weesunghyun <wsh05@snu.ac.kr>
2025-09-05 15:03:49 +0900

    medmnist 28x28

--- src/main_direct.py
+++ src/main_direct.py
@@ -439,6 +439,8 @@ class ExperimentDesign:
         self._set_dataloader()
         self._set_model()
         self._replace()
+		self.logger.info(self.model_teacher)
+		self.logger.info(self.model)
         self._set_trainer()
 
     def _set_gpu(self):
